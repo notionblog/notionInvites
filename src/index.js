8704ee218ffee2ab5453b1489b9b0c05ff5c84ed
yudax42 <yudax42@gmail.com>
2022-06-06 21:15:07 +0100

    add permission

--- src/index.js
+++ src/index.js
@@ -4,46 +4,50 @@ import {
   getSpace,
   inviteGuestsToSpace,
 } from "../lib/notion";
+
+const res = {
+  json(data, status) {
+    return new Response(JSON.stringify({ data }), {
+      status,
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+  },
+};
+
 export default {
   async fetch(request) {
     const { pathname, searchParams } = new URL(request.url);
+
+    // request params
     const email = searchParams.get("email");
     const workspace = searchParams.get("workspace");
-    if (pathname === "/invite" && email && workspace) {
-      // get space id
-      const space = await getSpace(workspace);
-      if (!space) {
-        return new Response(JSON.stringify({ error: "workspace not found" }), {
-          status: 404,
-          headers: {
-            "content-type": "application/json",
-          },
-        });
-      }
+    const pageid = searchParams.get("pageid");
+    const permission = searchParams.get("permission");
 
-      let user = await findUser(email);
+    if (pathname === "/invite" && email && workspace && pageid && permission) {
+      try {
+        // get space id
+        const space = await getSpace(workspace);
+        if (!space) {
+          return res.json({ error: "workspace not found" }, 404);
+        }
 
-      // if user is found
-      if (user && Object.keys(user).length !== 0) {
-        user = user.value.value.id;
-      } else {
-        // create user
-        const { userId } = await createEmailUser(email);
-        user = userId;
+        let user = await findUser(email);
+
+        if (user && Object.keys(user).length !== 0) {
+          user = user.value.value.id;
+        } else {
+          const { userId } = await createEmailUser(email);
+          user = userId;
+        }
+        await inviteGuestsToSpace(pageid, space, user, permission);
+        return res.json({ success: "user Invited" }, 200);
+      } catch (err) {
+        res.json({ error: err.stack }, 500);
       }
-      // invite the user
-      const inviteUser = await inviteGuestsToSpace(
-        "44cfd713a4ae42e893de3ba2d48516da",
-        space,
-        user
-      );
-      return new Response(JSON.stringify(inviteUser), {
-        headers: {
-          "content-type": "application/json",
-        },
-      });
     }
-
-    return new Response("Hello World!");
+    return res.json({ message: "script is running!" }, 200);
   },
 };
