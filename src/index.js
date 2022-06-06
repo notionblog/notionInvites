e0b321a43eb64549778edddba0532bd06fb810f5
yudax42 <yudax42@gmail.com>
2022-06-06 23:21:10 +0100

    fix env variable and permissions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,99 @@
-import {
-  findUser,
-  createEmailUser,
-  getSpace,
-  inviteGuestsToSpace,
-} from "../lib/notion";
+const _BASEURL = "https://www.notion.so/api/v3";
+
+function _HEADERS(env) {
+  return {
+    Cookie: `token_v2=${
+      typeof env.TOKEN_V2 !== undefined ? env.TOKEN_V2 : ""
+    };`,
+    "Content-Type": "application/json",
+  };
+}
+const _id2uuid = (id) =>
+  `${id.substr(0, 8)}-${id.substr(8, 4)}-${id.substr(12, 4)}-${id.substr(
+    16,
+    4
+  )}-${id.substr(20)}`;
+
+const getSpace = async (name, env) => {
+  const res = await fetch(`${_BASEURL}/getSpaces`, {
+    method: "POST",
+    headers: _HEADERS(env),
+  });
+  const data = await res.json();
+  const userSpaces = data[Object.keys(data)[0]].space;
+  const userSpacesIds = Object.keys(userSpaces);
+
+  return userSpacesIds.find(
+    (spaceId) =>
+      userSpaces[spaceId].value.name.toLowerCase() === name.toLowerCase()
+  );
+};
+
+const createEmailUser = async (email, env) => {
+  const body = {
+    email: email,
+    preferredLocaleOrigin: "inferred_from_inviter",
+    preferredLocale: "en-US",
+  };
+  const res = await fetch("https://www.notion.so/api/v3/createEmailUser", {
+    method: "POST",
+    headers: _HEADERS(env),
+    body: JSON.stringify(body),
+  });
+  const data = await res.json();
+
+  return data;
+};
+
+const inviteGuestsToSpace = async (
+  pageId,
+  spaceId,
+  userId,
+  permission,
+  env
+) => {
+  const permissions = {
+    edit: "read_and_write",
+    comment: "comment_only",
+    view: "reader",
+  };
+  console.log("permisi L ", permission);
+  const body = {
+    block: {
+      id: _id2uuid(pageId),
+      spaceId: spaceId,
+    },
+    permissionItems: [
+      {
+        type: "user_permission",
+        role: permissions[permission] || permissions["comment"],
+        user_id: userId,
+      },
+    ],
+  };
+  console.log(JSON.stringify(body));
+  const res = await fetch("https://www.notion.so/api/v3/inviteGuestsToSpace", {
+    headers: _HEADERS(env),
+    body: JSON.stringify(body),
+    method: "POST",
+  });
+  const data = await res.text();
+
+  return data;
+};
+
+const findUser = async (email, env) => {
+  const body = { email: email };
+
+  const res = await fetch("https://www.notion.so/api/v3/findUser", {
+    headers: _HEADERS(env),
+    body: JSON.stringify(body, env),
+    method: "POST",
+  });
+  const data = await res.json();
+
+  return data;
+};
 
 const res = {
   json(data, status) {
@@ -33,51 +123,53 @@ const getData = async (request, contentType) => {
   }
 };
 export default {
-  async fetch(request) {
-    const { headers } = request;
-    const { pathname, searchParams } = new URL(request.url);
-    const contentType = headers.get("content-type") || "";
-
-    // request params
-    let email = searchParams.get("email");
-    let workspace = searchParams.get("workspace");
-    let pageid = searchParams.get("pageid");
-    let permission = searchParams.get("permission");
-    if (pathname === "/gumroad") {
-      console.log("from gumroad");
-
-      const gumroadData = await getData(request, contentType);
-      const { email: userEmail, product_permalink } = JSON.parse(gumroadData);
-      const parsePageId = product_permalink.split("/")[4];
-      email = userEmail;
-      pageid = parsePageId;
+  async fetch(request, env) {
+    if (typeof env.TOKEN_V2 === "undefined") {
+      return res.json({ error: "TOKEN_V2 is required" }, 401);
     }
+    if (request.method === "POST") {
+      const { headers } = request;
+      const { pathname, searchParams } = new URL(request.url);
+      const contentType = headers.get("content-type") || "";
 
-    const paths = ["/invite", "/gumroad"];
-    console.log(`----- ${email} -- ${workspace} --- ${pageid}`);
+      // request params
+      let email = searchParams.get("email");
+      let workspace = searchParams.get("workspace");
+      let pageid = searchParams.get("pageid");
+      let permission = searchParams.get("permission");
+      if (pathname === "/gumroad") {
+        const gumroadData = await getData(request, contentType);
+        const { email: userEmail, product_permalink } = JSON.parse(gumroadData);
+        const parsePageId = product_permalink.split("/")[4];
+        email = userEmail;
+        pageid = parsePageId;
+      }
 
-    if (paths.includes(pathname) && email && workspace && pageid) {
-      try {
-        // get space id
-        const space = await getSpace(workspace);
-        if (!space) {
-          return res.json({ error: "workspace not found" }, 404);
+      const paths = ["/invite", "/gumroad"];
+
+      if (paths.includes(pathname) && email && workspace && pageid) {
+        try {
+          const space = await getSpace(workspace, env);
+          if (!space) {
+            return res.json({ error: "workspace not found" }, 404);
+          }
+
+          let user = await findUser(email, env);
+
+          if (user && Object.keys(user).length !== 0) {
+            user = user.value.value.id;
+          } else {
+            const { userId } = await createEmailUser(email, env);
+            user = userId;
+          }
+          await inviteGuestsToSpace(pageid, space, user, permission, env);
+          return res.json({ success: "user Invited" }, 200);
+        } catch (err) {
+          res.json({ error: err.stack }, 500);
         }
-
-        let user = await findUser(email);
-
-        if (user && Object.keys(user).length !== 0) {
-          user = user.value.value.id;
-        } else {
-          const { userId } = await createEmailUser(email);
-          user = userId;
-        }
-        await inviteGuestsToSpace(pageid, space, user, permission);
-        return res.json({ success: "user Invited" }, 200);
-      } catch (err) {
-        res.json({ error: err.stack }, 500);
+      } else {
+        return res.json({ message: "script is running!" }, 200);
       }
     }
-    return res.json({ message: "script is running!" }, 200);
   },
 };
