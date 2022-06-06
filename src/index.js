c8f636158508508481d6fcb5c1ff0f6f475b5dde
yudax42 <yudax42@gmail.com>
2022-06-06 17:59:35 +0100

    find user

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
+import { findUser } from "../lib/notion";
 export default {
   async fetch(request) {
+    const { pathname, searchParams } = new URL(request.url);
+    if (pathname === "/invite") {
+      const email = searchParams.get("email");
+      console.log("email: ", email);
+      const user = await findUser(email);
+      console.log("user: ", user);
+      if (user && Object.keys(user).length !== 0) {
+        return new Response(JSON.stringify(user), {
+          headers: {
+            "content-type": "application/json",
+          },
+        });
+      }
+    }
+
     return new Response("Hello World!");
   },
 };
